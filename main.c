667895865d533519ab0248731c3b26760016c371
Antti Yli-Kujala <a.ylikujala@gmail.com>
2022-09-21 18:35:53 +0300

    fixing stuff

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <time.h>
 #include "sortfunctions.h"
 
 
@@ -12,17 +13,45 @@ void printArr(int* arr, int size){
 
 int main(int argc, char const *argv[])
 {
-    time_t t;
     int size = 100;
     int arr[size];
+    char input[100];
+    int isRunning = 1;
     for(int i = 0; i < size; i++){
         arr[i] = (rand() % 1000);
     }
+    while(isRunning){
+        fgets(input, 100, stdin);
+        switch(input[0]){
 
-    printArr(arr, size);
-    shellSort(arr, size);
-    printf("\n");
-    printArr(arr, size);
+            case'A':
+                for(int i = 0; i < size; i++){
+                    arr[i] = (rand() % 1000);
+                }
+                
+                break;
+
+            case'P':
+                printArr(arr, size);
+                break;
+            
+            case'S':
+                clock_t start = clock();
+                shellSort(arr, size);
+                clock_t end = clock();
+                double time = (double)(end - start) / CLOCKS_PER_SEC;
+                printf("\nShellsort execution time: %f\n", time);
+                break;
+
+
+            case'Q':
+                isRunning = 0;
+                break;
+
+            default:
+                printf("\nA : reshuffles array\nP : print array\nS : shell sort\nQ : quit\n");
+        }
+    }
 
     return 0;
 }
